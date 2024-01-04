b757fb1ba56b334ae5eb85b340b9cf03c4d15278
David <121982969+Oxdavik@users.noreply.github.com>
2024-01-04 23:28:01 +0100

    Delete class directory

--- class/DeplacementsIncorrectes.java
+++
@@ -1,6 +0,0 @@
-public class DeplacementsIncorrectes extends Exception{
-    public DeplacementsIncorrectes(String msg){
-        super(msg);
-    }
-    
-}
--- class/Teleportation.java
+++
@@ -1,5 +0,0 @@
-public interface Teleportation {
-public void TP(int x,int y,Grille g) throws CoordonneesIncorrectesException,CaseNonPleineException;
-
-    
-}
